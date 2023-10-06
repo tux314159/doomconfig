6f936247a47bf73aeebd4acf2046fda7e396cd2a
Isaac <tux314159@gmail.com>
2023-10-07 01:57:53 +0800

    auto

--- flycheck_vim-number-inc-dec.el
+++ flycheck_vim-number-inc-dec.el
@@ -17,11 +17,11 @@
     ;; Search for a number
     (while (and (not-nl (char-after)) (not (is-digit (char-after))))
       (forward-char)))
-  (setq! num-start-point (point))
+  (setq num-start-point (point))
   ;; Find the end of the number
   (while (and (is-digit (char-after)) (not-nl (char-after)))
     (forward-char))
-  (setq! num-end-point (point))
+  (setq num-end-point (point))
   ;; Ensure we actually found a number
   (if (= num-start-point num-end-point)
       'nil
@@ -31,14 +31,14 @@
   (string-to-number (buffer-substring (car pts) (cdr pts))))
 
 (defun inc-num-lit ()
-  (setq! pts (find-num-lit))
+  (setq pts (find-num-lit))
   (if pts
       (let ((len (- (cdr pts) (car pts)))
             (new-num (number-to-string (+ 1 (get-num-lit pts)))))
         (setq new-num  ; add 0-padding if needed
-               (concat
-                (make-string (max 0 (- len (length new-num))) ?0)
-                new-num))
+              (concat
+               (make-string (max 0 (- len (length new-num))) ?0)
+               new-num))
         ;; Delete old number and insert new one
         (delete-region (car pts) (cdr pts))
         (goto-char (car pts))
