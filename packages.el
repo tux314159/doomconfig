6141465621442f1c2a4c017ad643810c4e023c85
Isaac <tux314159@gmail.com>
2023-10-06 21:25:16 +0800

    auto

--- packages.el
+++ packages.el
@@ -8,6 +8,8 @@
 
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;(package! some-package)
+(package! tree-sitter)
+(package! tree-sitter-langs)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
