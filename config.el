2ba8d59dab57ad039dc00309ac8e8d0b2267a77a
Isaac <tux314159@gmail.com>
2023-10-06 22:35:59 +0800

    auto

--- config.el
+++ config.el
@@ -21,7 +21,7 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "JetBrains Mono" :size 16 :weight 'normal :spacing 110))
+(setq doom-font (font-spec :family "JetBrains Mono" :size 16 :weight 'normal :spacing 0))
 
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
