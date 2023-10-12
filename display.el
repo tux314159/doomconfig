2b95752a32d79e058b36ee0e6f2a929b8ba33039
Isaac <tux314159@gmail.com>
2023-10-12 14:52:05 +0800

    auto

---
+++ display.el
@@ -0,0 +1,8 @@
+;;; display.el -*- lexical-binding: t; -*-
+
+(setq grapher-global-framebuffer-width 256)
+(setq grapher-global-framebuffer-height 90)
+;; Create global framebuffer
+(setq grapher-global-framebuffer (make-vector grapher-global-framebuffer-height nil))
+
+(setq grapher-global-framebuffer (make-vector grapher-global-framebuffer-height nil))
