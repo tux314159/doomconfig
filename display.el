dd49cac1b8c6d98147999be1940238cdfda33171
Isaac <tux314159@gmail.com>
2023-10-12 14:54:50 +0800

    auto

--- display.el
+++ display.el
@@ -1,8 +1,11 @@
 ;;; display.el -*- lexical-binding: t; -*-
 
+(load "~/.config/doom/util")
+
 (setq grapher-global-framebuffer-width 256)
 (setq grapher-global-framebuffer-height 90)
 ;; Create global framebuffer
 (setq grapher-global-framebuffer (make-vector grapher-global-framebuffer-height nil))
-
+(loopn i grapher-global-framebuffer-height
+       (aset (aref grapher-global-framebuffer n))
 (setq grapher-global-framebuffer (make-vector grapher-global-framebuffer-height nil))
