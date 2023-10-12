31f95a5540fea7700024db2a4ce123c2d543758a
Isaac <tux314159@gmail.com>
2023-10-12 15:36:41 +0800

    auto

--- display.el
+++ display.el
@@ -1,11 +1,44 @@
 ;;; display.el -*- lexical-binding: t; -*-
+;;; NOTE: braille chars are 2x3
 
 (load "~/.config/doom/util")
 
-(setq grapher-global-framebuffer-width 256)
-(setq grapher-global-framebuffer-height 90)
+(setq grapher-global-frame-width 128)
+(setq grapher-global-frame-height 45)
+(setq grapher-global-framebuf-width (* 2 grapher-global-frame-width))
+(setq grapher-global-framebuf-height (* 3 grapher-global-frame-height))
 ;; Create global framebuffer
-(setq grapher-global-framebuffer (make-vector grapher-global-framebuffer-height nil))
-(loopn i grapher-global-framebuffer-height
-       (aset grapher-global-framebuffer i (make-vector grapher-global-framebuffer-width 0)))
-(setq grapher-global-framebuffer (make-vector grapher-global-framebuffer-height nil))
+(setq grapher-global-framebuf (make-vector grapher-global-framebuf-height nil))
+(loopn i grapher-global-framebuf-height
+       (aset grapher-global-framebuf i (make-vector grapher-global-framebuf-width nil)))
+
+;; Framebuffer primitives
+(defun grapher-prepare-screen ()
+  "Clear the buffer and draw enough spaces"
+  (let ((blankl (make-string grapher-x-size ? )))
+    (loopn _ (/ grapher-global-framebuffer-width 2)
+           (insert blankl)
+           (insert (c2s ?\n)))))
+
+(defun grapher-global-framebuf-on (pos)
+  "Turn on a pixel"
+  (let ((x (car pos))
+        (y (cdr pos)))
+    (aset (aref grapher-global-framebuffer y) x t)))
+
+(defun grapher-global-framebuf-off (pos)
+  "Turn off a pixel"
+  (let ((x (car pos))
+        (y (cdr pos)))
+    (aset (aref grapher-global-framebuf y) x nil)))
+
+(defun grapher--framebuf-condense-cell (pos)
+  "Condense a group of framebuffer cells into a 2x3 cell at pos."
+  (let ((fb-x (* 2 (car pos)))
+        (fb-y (* 3 (cdr pos)))
+        (braille '( ⠁ ⠃ ⠉ ⠙ ⠑ ⠋ ⠛ ⠓ ⠊ ⠚ ⠈ ⠘
+                    ⠄ ⠅ ⠇ ⠍ ⠝ ⠕ ⠏ ⠟ ⠗ ⠎ ⠞ ⠌ ⠜
+                    ⠤ ⠥ ⠧ ⠭ ⠽ ⠵ ⠯ ⠿ ⠷ ⠮ ⠾ ⠬ ⠼
+                    ⠠ ⠡ ⠣ ⠩ ⠹ ⠱ ⠫ ⠻ ⠳ ⠪ ⠺ ⠨ ⠸
+                    ⠀ ⠂ ⠆ ⠒ ⠲ ⠢ ⠖ ⠶ ⠦ ⠔ ⠴ ⠐ ⠰))
+   (
