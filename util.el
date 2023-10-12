c0788a68f5e46c47909263a00c8ace648cfdb6a5
Isaac <tux314159@gmail.com>
2023-10-12 14:52:02 +0800

    auto

---
+++ util.el
@@ -0,0 +1,23 @@
+;;; util.el -*- lexical-binding: t; -*-
+
+(defmacro loopn (ivar n &rest body)
+  "Loop n times"
+  `(let ((,ivar 0))
+     (while (< ,ivar ,n)
+       (progn
+         ,@body
+         (setq ,ivar (+ ,ivar 1))))))
+
+(defun c2s (c)
+  "Convert character to a string"
+  (make-string 1 c))
+
+(defun overwrite-char (c)
+  (delete-char 1)
+  (insert (c2s c)))
+
+(defun succ (n)
+  (+ n 1))
+
+(defun pred (n)
+  (+ n 1))
